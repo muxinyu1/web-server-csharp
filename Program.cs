9f68aa68a2c2295ca30b9623aec309c17cacfd46
mxy <13614169547@163.com>
2023-07-03 17:38:50 +0800

    添加注释

--- Program.cs
+++ Program.cs
@@ -10,29 +10,40 @@ namespace WebServer
         private const int Max = 128;
         public static void Main()
         {
+            // 创建套接字
             var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            // 创建接受端口, IPAddress.Any表示允许来自任何IP的连接
             var endPoint = new IPEndPoint(IPAddress.Any, 8888);
+            // 绑定
             socket.Bind(endPoint);
+            // 持续监听, Max是最大TCP连接数
             socket.Listen(Max);
             while (true)
             {
                 var newSocket = socket.Accept();
+                // 只要有新的请求就创建一个线程处理
                 var thread = new Thread(() => HandleClient(newSocket));
                 thread.Start();
             }
         }
 
+        /// <summary>
+        /// 处理Tcp连接
+        /// </summary>
+        /// <param name="socket">连接的Tcp Socket</param>
         private static void HandleClient(Socket socket)
         {
             var buffer = new byte[2048];
 
             while (true)
             {
+                // 读取HTTP请求
                 var bytesRead = socket.Receive(buffer);
                 if (bytesRead == 0)
                 {
                     break;
                 }
+                // 将请求字节数组按照ASCII来解释(因为HTTP报文是ASCII编码的)
                 var httpRequestString = Encoding.ASCII.GetString(buffer, 0, bytesRead);
                 // Console.WriteLine(httpRequestString);
                 try
@@ -44,9 +55,13 @@ namespace WebServer
                     Console.WriteLine(HttpHelper.MakeLog(httpRequestMessage, httpResponseMessage, socket));
                     
                     Debug.Assert(httpResponseMessage.Content.Headers.ContentType != null);
+                    
+                    // 将HttpResponseMessage对象转为string便于发送
                     var httpResponseMessageString = httpResponseMessage.ToHttpResponseMessageString();
                     var bytesToWrite = Encoding.UTF8.GetBytes(httpResponseMessageString);
+                    // 发送Http响应报文
                     socket.Send(bytesToWrite);
+                    // 如果客户端没有要求持续连接, 就释放Tcp连接
                     if (httpRequestMessage.Headers.ConnectionClose == true)
                     {
                         break;
@@ -57,7 +72,7 @@ namespace WebServer
                     Console.WriteLine(e);
                 }
             }
-
+            // 如果退出上面的循环, 说明可以关闭Tcp连接了
             socket.Close();
         }
     }
